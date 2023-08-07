60035f80b83f0d6aee7739051509259d24906eec
Patria <patria.lyx@gmail.com>
2023-08-08 01:57:09 +0800

    change back to cpu so it can work with cpu

--- services/EDU-Segmentation-API/run_segbot.py
+++ services/EDU-Segmentation-API/run_segbot.py
@@ -1,5 +1,5 @@
 from edu_segmentation import download, main
 
-def main_input_output(sent, granularity_level="default", model="bart"):
+def main_input_output(sent, granularity_level="default", model="bart", device='cpu'):
     download.download_models()
-    return main.run_segbot(sent, granularity_level=granularity_level, model=model, device='cpu')
+    return main.run_segbot(sent, granularity_level=granularity_level, model=model, device=device)
