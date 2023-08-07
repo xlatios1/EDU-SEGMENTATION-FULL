c9af3947b25e89ec198ed3f74383f9d563b2cd0b
Patria <patria.lyx@gmail.com>
2023-08-08 00:37:23 +0800

    updated loading message

--- client/src/components/Loading.jsx
+++ client/src/components/Loading.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Loading = () => {
   return (
     <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
-      <h2>Loading...</h2>
+      <h2>Analyzing the food reviews... this may take a while...</h2>
     </div>
   );
 };
