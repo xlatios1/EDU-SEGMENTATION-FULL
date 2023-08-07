08f3b789aeb745a72df48a51ba40640054990054
Patria <patria.lyx@gmail.com>
2023-08-07 23:32:09 +0800

    include new parameter device

--- services/EDU-Segmentation-API/run_segbot.py
+++ services/EDU-Segmentation-API/run_segbot.py
@@ -2,4 +2,4 @@ from edu_segmentation import download, main
 
 def main_input_output(sent, granularity_level="default", model="bart"):
     download.download_models()
-    return main.run_segbot(sent, granularity_level=granularity_level, model=model)
+    return main.run_segbot(sent, granularity_level=granularity_level, model=model, device='cpu')
